402cbabe6fd0dd8af5afd6e863a28ac6f7a8a7f4
Michael <111774557+MichaelKuch26@users.noreply.github.com>
2023-02-23 00:22:29 +0400

    HW3

--- src/main/java/org/example/seminar3/hw3/model/Items.java
+++ src/main/java/org/example/seminar3/hw3/model/Items.java
@@ -1,2 +1,14 @@
-package org.example.seminar3.hw3.model;public class Items {
+package org.example.seminar3.hw3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Items {
+    private String name;
+    private String surname;
+    private Integer price;
+    private Integer year;
+    private Integer pages;
 }
